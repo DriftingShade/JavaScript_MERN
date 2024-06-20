1c3da49dc9e0047ea93bca7ccbed881dbad59a7e
DriftingShade <sarus333@yahoo.com>
2024-06-20 10:45:25 -0600

    Putting it together assignment

---
+++ 02_ReactJS/putting-it-together/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import PersonCard from './components/PersonCard'
+
+function App() {
+
+  return (
+    <>
+      <PersonCard firstName="Shane" lastName="Nosack" initialAge={33} hairColor="Brown" />
+      <PersonCard firstName="Bob" lastName="Evans" initialAge={84} hairColor="Bald" />
+    </>
+  )
+}
+
+export default App
---
+++ 02_ReactJS/putting-it-together/src/components/PersonCard.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+const PersonCard = ({ firstName, lastName, initialAge, hairColor }) => {
+    const [age, setAge] = useState(initialAge);
+
+    const birthdayButton = () => {
+        setAge(age + 1);
+    }
+
+    return (
+        <>
+        <h2>{lastName}, {firstName}</h2>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hairColor}</p>
+        <button onClick={birthdayButton}>Birthday Button for {firstName} {lastName}</button>
+        </>
+    )
+}
+
+export default PersonCard
